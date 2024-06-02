019844e7f658a1ac594b517913bd009fbad14255
Abdulmalik <ayodejiamzat@gmail.com>
2024-06-02 15:40:23 +0100

    auto-commit

--- src/Components/Myresume.js
+++ src/Components/Myresume.js
@@ -71,7 +71,7 @@ const Myresume = (props) => {
                 <svg fill={prop1 && prop1.length > 0 ? prop1 : hcolor ? hcolor : "rgb(20, 223, 166)"} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20 12.875v5.068c0 2.754-5.789 4.057-9 4.057-3.052 0-9-1.392-9-4.057v-6.294l9 4.863 9-3.637zm-8.083-10.875l-12.917 5.75 12 6.5 11-4.417v7.167h2v-8.25l-12.083-6.75zm13.083 20h-4c.578-1 1-2.5 1-4h2c0 1.516.391 2.859 1 4z"/></svg>
                 <p className='text-white mreq'>Educational Qualification</p>
               </div>
-              <div className='d-flex justify-content-start mrmov' data-aos="fade-up" data-aos-duration="1000">
+              {/* <div className='d-flex justify-content-start mrmov' data-aos="fade-up" data-aos-duration="1000">
                 <div className='mrdur' style={{backgroundColor:`${prop2 && prop2.length > 0 ? prop2 : hcolor2 ? hcolor2 : "rgba(20, 223, 166, 0.171)"}`, borderRadius: typeof prop3 === "number" ? prop3 + "px" : typeof hrad === "number" ? hrad + "px" : "10px"}}><p>2006 - 2010</p></div>
                 <hr className='hrrr' style={{width: "30px", height: "4px"}}></hr>
                 <div className='hrd'>
@@ -85,7 +85,7 @@ const Myresume = (props) => {
                   <p className='mrp2'>Air Force Primary School Ibadan</p>
                   <p className='mrp3'>Received standard primary education and developed strong foundation in academics and personal development.</p>
                 </div>
-              </div>
+              </div> */}
               <div className='d-flex justify-content-start mrmo' data-aos="fade-up" data-aos-duration="1000">
                 <div className='mrdur' style={{backgroundColor:`${prop2 && prop2.length > 0 ? prop2 : hcolor2 ? hcolor2 : "rgba(20, 223, 166, 0.171)"}`, borderRadius: typeof prop3 === "number" ? prop3 + "px" : typeof hrad === "number" ? hrad + "px" : "10px"}}><p>2011 - 2017</p></div>
                 <hr className='hrrr' style={{width: "30px", height: "4px"}}></hr>
